41f838a3ccc68c0d231737d4e213d2251530922b
Vanessa Gutierrez <122655134+nessakura-eng@users.noreply.github.com>
2026-03-10 19:24:11 -0400

    Update HomePage.java

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,28 +1,80 @@
+package pages;
+
 import org.openqa.selenium.By;
 
 public class HomePage extends BasePage {
 
-By searchBox = By.id("searchInput");
-By searchButton = By.cssSelector("button[type='submit']");
-By englishLink = By.id("js-link-box-en");
+    // Locators
 
-public void open(){
+    By searchBox = By.id("searchInput");
+    By searchButton = By.cssSelector("button[type='submit']");
+    By englishLanguageLink = By.id("js-link-box-en");
+    By donateLink = By.linkText("Donate");
+    By footer = By.className("footer");
 
-driver.get("https://www.wikipedia.org/");
+    // Open homepage
 
-}
+    public void open() {
 
-public void search(String text){
+        driver.get("https://www.wikipedia.org/");
 
-driver.findElement(searchBox).sendKeys(text);
-driver.findElement(searchButton).click();
+    }
 
-}
+    // Enter text into search box
 
-public void openEnglish(){
+    public void enterSearch(String text) {
 
-driver.findElement(englishLink).click();
+        driver.findElement(searchBox).sendKeys(text);
 
-}
+    }
+
+    // Click search button
+
+    public void clickSearch() {
+
+        driver.findElement(searchButton).click();
+
+    }
+
+    // Full search action
+
+    public void search(String text) {
+
+        enterSearch(text);
+        clickSearch();
+
+    }
+
+    // Click English language
+
+    public void openEnglishSite() {
+
+        driver.findElement(englishLanguageLink).click();
+
+    }
+
+    // Verify search box exists
+
+    public boolean isSearchBoxDisplayed() {
+
+        return driver.findElement(searchBox).isDisplayed();
+
+    }
+
+    // Verify Donate link exists
+
+    public boolean isDonateLinkDisplayed() {
+
+        return driver.findElement(donateLink).isDisplayed();
+
+    }
+
+    // Verify footer exists
+
+    public boolean isFooterDisplayed() {
+
+        return driver.findElement(footer).isDisplayed();
+
+    }
 
 }
